d406b29573ac332f3080a744810e9fa579d84bda
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-16 15:52:42 +0200

    created mock database and get method/post method

--- Routes/users.js
+++ Routes/users.js
@@ -3,8 +3,32 @@ import express, { request } from 'express';
 // initialising router
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('Hello');
+// Mock database 
+const users = [
+    {
+        firstName: "John",
+        lasttName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Joane",
+        lasttName: "Doe",
+        age: 24
+    }
+]
+
+// gives all the users at /users 
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send(users);
+});
+
+// post route
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(`User with the name ${user.firstName} added to the database`);
+
 });
 
 export default router;
