9fab27da2c75fb85ef11244a520b12ea2aa8b0bf
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-16 20:19:06 +0200

    imported all logic from controller and replaced callback

--- Routes/users.js
+++ Routes/users.js
@@ -1,74 +1,31 @@
-import express, { request } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
+import { 
+    createUser, 
+    getUsers, 
+    getUser , 
+    updateUser, 
+    deleteUser 
+} from '../controllers/users.js';
 
 // initialising router
 const router = express.Router();
 
-// Mock database 
-let users = [
-    {
-        firstName: "John",
-        lasttName: "Doe",
-        age: 25,
-        id : uuidv4()
-    },
-    {
-        firstName: "Joane",
-        lasttName: "Doe",
-        age: 24,
-        id : uuidv4()
-    }
-]
-
 // gives all the users at /users 
-router.get('/', (req, res) => {
-    console.log(users);
-    res.send(users);
-});
+router.get('/', getUsers);
 
 // post route
 // added uuid to handle a unique id
 // this uses req.params to get the value
-router.post('/', (req, res) => {
-    const user = req.body;
-    users.push({...user, id: uuidv4()});
-    res.send(`User with the name ${user.firstName} added to the database`);
-});
+router.post('/', createUser);
 
 // route for specific user by id 
-router.get('/:id', (req, res)=>{
-    const { id }= req.params;
-
-    const foundUser = users.find((user)=> user.id == id);
-
-    res.send(foundUser);
-});
+router.get('/:id', getUser);
 
 // delete route to delete from id
-router.delete('/:id', (req, res) => {
-    const { id }= req.params;
-
-    users = users.filter((user)=> user.id != id);
-
-    res.send(`User with the id ${id} was deleted from the database!`);
-})
+router.delete('/:id', deleteUser);
 
 
 //patching route
-router.patch('/:id', (req, res)=> {
-    const { id } = req.params;
-    const { firstName, lastName, age }= req.body;
-    const user = users.find((user) => user.id == id);
+router.patch('/:id', updateUser);
 
-    if (firstName) {
-        user.firstName = firstName;
-    }
-    if (lastName) {
-        user.lastName = lastName;
-    }
-    if (age) {
-        user.age = age;
-    }
-    res.send(user);
-})
 export default router;
